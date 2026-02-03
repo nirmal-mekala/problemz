f9798436ed3fdf171ca8736e4c99d3a9e9f89c60
Nirmal Mekala <ndmekala@icloud.com>
2026-02-02 21:51:32 -0600

    got there

--- medium/longest-unique-substring/working.ts
+++ medium/longest-unique-substring/working.ts
@@ -1,7 +1,20 @@
 export function longestUniqueSubstringLength(input: string): number {
-  if (input.length === 0) {
-    return 0;
-  }
+	console.log(`INPUT: ${input}`);
+	let maxLength = 0;
+	const seen = new Map<string, number>();
+	let start = 0;
 
-  return 1;
+	for (let i = 0; i < input.length; i++) {
+		const char = input[i];
+
+		if (seen.get(char) !== undefined && seen.get(char)! >= start) {
+			start = seen.get(char)! + 1;
+		}
+
+		seen.set(char, i);
+		const windowLength = i - start + 1;
+		maxLength = Math.max(windowLength, maxLength);
+	}
+
+	return maxLength;
 }
