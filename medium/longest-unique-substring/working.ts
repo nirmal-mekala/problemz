b4c8cf59e7711a783149b023a7c88c4abd8f0db0
Nirmal Mekala <ndmekala@icloud.com>
2026-02-11 07:21:10 -0600

    re-do longest unique substring

--- medium/longest-unique-substring/working.ts
+++ medium/longest-unique-substring/working.ts
@@ -1,19 +1,46 @@
 export function longestUniqueSubstringLength(input: string): number {
-	let maxLength = 0;
+	let max = 0;
+	let windowStart = 0;
 	const seen = new Map<string, number>();
-	let start = 0;
-
 	for (let i = 0; i < input.length; i++) {
 		const char = input[i];
 
-		if (seen.get(char) !== undefined && seen.get(char)! >= start) {
-			start = seen.get(char)! + 1;
+		if (seenWithinWindow({ seen, windowStart, char })) {
+			// move windowStart to after breaking character
+			windowStart = seen.get(char)! + 1;
 		}
-
+		// update max
+		max = Math.max(max, i - windowStart + 1);
+		// update seen
 		seen.set(char, i);
-		const windowLength = i - start + 1;
-		maxLength = Math.max(windowLength, maxLength);
 	}
-
-	return maxLength;
+	return max;
 }
+
+const seenWithinWindow = (payload: {
+	seen: Map<string, number>;
+	windowStart: number;
+	char: string;
+}) => {
+	return (
+		payload.seen.has(payload.char) &&
+		(payload.seen.get(payload.char) ?? -Infinity) >= payload.windowStart
+	);
+};
+
+/*
+  *
+  { input: "", expected: 0 },
+  { input: "a", expected: 1 },
+  { input: "abcabcbb", expected: 3 },
+  { input: "bbbbb", expected: 1 },
+  { input: "pwwkew", expected: 3 },
+  { input: "dvdf", expected: 3 },
+
+
+
+
+
+  { input: "aab", expected: 2 },
+  { input: "anviaj", expected: 5 }
+  * */
